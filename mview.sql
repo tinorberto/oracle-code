a23bb01693ea2a01e746bdaa2e43a923cdd16b05
Tiago da Costa Norberto <tiago.cnorberto@pbh.gov.br>
2017-06-21 08:42:13 -0300

    Mview scripts

---
+++ mview.sql
@@ -0,0 +1,13 @@
+ 
+-- Forcar o refresh  
+begin
+	--dbms_mview.refresh('bhmap.bhmap_area_trecho_logradouro');
+	dbms_mview.refresh('bhmap.BHMAP_AREA_TRECHO_LOGRADOURO');
+    -- DBMS_SNAPSHOT.REFRESH('bhmap.bhmap_obra_trecho_logradouro');
+end;
+
+
+  
+  
+  
+  
